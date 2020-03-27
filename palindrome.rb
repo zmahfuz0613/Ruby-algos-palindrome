80b3d6ba280f773a94fb78507c3bc9ef47a94f39
David Whitlatch <davidtwhitlatch@gmail.com>
2020-03-27 00:55:54 -0400

    updated README

--- palindrome.rb
+++ palindrome.rb
@@ -1,8 +1,8 @@
 # write your sudo code here
 
-def palindrome(str)
+def palindrome?(str)
   #your code here
   
 end
 
-p palindrome "a man a plan a canal panama"
+p palindrome? "a man a plan a canal panama"
--- spec/palindrome_spec.rb
+++ spec/palindrome_spec.rb
@@ -1,18 +1,18 @@
 require_relative "../palindrome.rb"
 
 
-describe "palindrome" do
+describe "palindrome?" do
 
   it "check if word is a palindrome" do
-    expect palindrome("eye").to eq(true)
+    expect palindrome?("eye").to eq(true)
   end
 
   it "check if word with spaces is a palindrome" do
-    expect palindrome("a man a plan a canal panama").to eq(true)
+    expect palindrome?("a man a plan a canal panama").to eq(true)
   end
 
   it "check if word isn't a palindrome" do
-    expect palindrome("eyed").to eq(false)
+    expect palindrome?("eyed").to eq(false)
   end
 
 end
