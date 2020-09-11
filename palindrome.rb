fdd3a9f6e8ac3b6549babb0a5e734ba539ab303e
Zunayed Mahfuz <zunayed.eshna@gmail.com>
2020-09-11 10:37:38 -0400

    Completed

--- palindrome.rb
+++ palindrome.rb
@@ -1,11 +1,13 @@
 # RUBY PALINDROME
 
-def palindrome(str)
- 
+def palindrome?(str)
+  str = str.downcase
+
+  str = str.delete(' ')
+
+  str === str.reverse
 end
 
-puts palindrome("a man a plan a canal panama")
-
-
+puts palindrome?('a man a plan a canal panama')
 
 # WOP
